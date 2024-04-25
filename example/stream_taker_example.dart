5d99f30b026b1eaa69b1340d14bd530418968582
AoEiuV020 <aoeiuv020@gmail.com>
2024-04-25 22:52:16 +0800

    create project

---
+++ example/stream_taker_example.dart
@@ -0,0 +1,6 @@
+import 'package:stream_taker/stream_taker.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/stream_taker_test.dart
@@ -0,0 +1,16 @@
+import 'package:stream_taker/stream_taker.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
